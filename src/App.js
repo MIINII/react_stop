ca45129ad7a38e7f08436c15044716a96de099f3
MIINII <ft.dawn.o@gmail.com>
2022-11-25 03:06:37 +0900

    :adhesive_bandage: Fix : UserList.jsx key값 추가

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUserList) => {
-      return [...prevUserList, { name: uName, age: uAge }];
+      return [...prevUserList, { name: uName, age: uAge, id: Math.random().toString() }];
     });
   };
 
